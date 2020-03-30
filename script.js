0c03c3c44f7e74bf8893daaf43e4375ff5b0756c
Ellin Tj <tjellin@gmail.com>
2020-03-29 20:35:52 -0700

    editing and adding lines to this file

--- script.js
+++ script.js
@@ -52,122 +52,21 @@ function start() {
 //   * Delete departments, roles, and employees
 //   * View the total utilized budget of a department -- ie the combined salaries of all employees in that department
 
-function enterEmployees() {
-    inquirer.prompt([
-        {
-            name: "EmpFirstLayer",
-            type: "rawlist",
-            message: "What would you like to do in the employee record?",
-            choices: [
-                "View employees",
-                "Add an employee",
-                "Update an employee"
-            ]
-        }
-    ]).then(answer => {
-        if (answer.EmpFirstLayer === "View employees") {
-            connection.query('SELECT * FROM employee', (err, items) => {
-                if (err) throw err;
-
-            });
-
-        } else if (answer.firstLayer === "Add an employee") {
-            
-            inquirer.prompt([
-                {
-                    name: "fName",
-                    type: "input",
-                    message: "What is the new employee's first name?"
-                },
-                {
-                    name: "lName",
-                    type: "input",
-                    message: "What is the new employee's last name?"
-                },
-                {
-                    name: "role",
-                    type: "rawlist",
-                    message: "What is the new employee's role?",
-                    choices: []
-                }, 
-                {
-                    name: "manager",
-                    type: "rawlist",
-                    message: "What is the new employee's manager?",
-                    choices: []
-                }
-            ]).then(ans => {
-
-            });
-        } else if (answer.firstLayer === "Update an employee") {
-        } else {
-            console.log("Sorry, your input is incorrect");
-        }
-
-
-
-
-        // connection.query("SELECT * FROM auctions WHERE id = ? ", id, (err, res) => {
-        //     const actualItem = res[0];
-        //     if (actualItem.highest_bid < parseInt(answer.bid)) {
-        //         connection.query('UPDATE auctions SET ? WHERE ?',
-        //             [
-        //                 {
-        //                     highest_bid: parseInt(answer.bid)
-        //                 },
-        //                 {
-        //                     id: actualItem.id
-        //                 }
-        //             ], function (err) {
-        //                 if (err) throw err;
-        //                 console.log("Bid was placed successfuly!");
-        //                 start();
-        //             }
-        //         )
-        //     } else {
-        //         console.log("Your bid was too low! Try again");
-        //         // call rebid here with the items id
-        //         reBid(actualItem.id);
-        //     }
-        // })
-    });
-}
 
 function enterDept() {
     inquirer.prompt([
-        {
-            name: "name",
-            type: "input",
-            message: "What is the name of the item you want to submit?"
-        },
-        {
-            name: "category",
-            type: "input",
-            message: "What category do you want your item in?"
-        },
-        {
-            name: "startingBid",
-            type: 'input',
-            message: "What would you like the starting bid to be?",
-            validate: function (value) {
-                if (isNaN(value) === false) {
-                    return true;
-                }
-                return false;
-            }
+        {            
+            name: "DeptFirstLayer",
+            type: "rawlist",
+            message: "What would you like to do in the department record?",
+            choices: [
+                "View departments",
+                "Add a department",
+                "Update a department",
+                "Delete a department"
+            ]
         }
     ]).then(answer => {
-        connection.query("INSERT INTO auctions SET ?",
-            {
-                item_name: answer.name,
-                category: answer.category,
-                starting_bid: answer.startingBid,
-                highest_bid: answer.startingBid
-            }, (err) => {
-                if (err) throw err;
-                console.log("Successfully entered your item");
-                start();
-            });
     })
 }
 
@@ -223,4 +122,108 @@ function enterRoles() {
             }
         })
     })
+}
+
+function enterEmployees() {
+    inquirer.prompt([
+        {
+            name: "EmpFirstLayer",
+            type: "rawlist",
+            message: "What would you like to do in the employee record?",
+            choices: [
+                "View employees",
+                "Add an employee",
+                "Update an employee"
+            ]
+        }
+    ]).then(answer => {
+        if (answer.EmpFirstLayer === "View employees") {
+            connection.query('SELECT * FROM employee', (err, items) => {
+                if (err) throw err;
+
+            });
+
+        } else if (answer.firstLayer === "Add an employee") {
+            
+            inquirer.prompt([
+                {
+                    name: "fName",
+                    type: "input",
+                    message: "What is the new employee's first name?",
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return false;
+                    }
+                },
+                {
+                    name: "lName",
+                    type: "input",
+                    message: "What is the new employee's last name?",
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return false;
+                    }
+                },
+                {
+                    name: "role",
+                    type: "rawlist",
+                    message: "What is the new employee's role?",
+                    choices: []
+                }, 
+                {
+                    name: "manager",
+                    type: "rawlist",
+                    message: "What is the new employee's manager?",
+                    choices: []
+                }
+            ]).then(ans => {
+                connection.query("INSERT INTO auctions SET ?",
+                    {
+                        first_name: ans.fName,
+                        lastName: ans.lName,
+                        role_id: answer.startingBid,
+                        manager_id: answer.startingBid
+                    }, (err) => {
+                        if (err) throw err;
+                        console.log("Successfully adding an employee");
+                        start();
+                    });
+
+            });
+        } else if (answer.firstLayer === "Update an employee") {
+        } else {
+            console.log("Sorry, your input is incorrect");
+        }
+
+
+
+
+        // connection.query("SELECT * FROM auctions WHERE id = ? ", id, (err, res) => {
+        //     const actualItem = res[0];
+        //     if (actualItem.highest_bid < parseInt(answer.bid)) {
+        //         connection.query('UPDATE auctions SET ? WHERE ?',
+        //             [
+        //                 {
+        //                     highest_bid: parseInt(answer.bid)
+        //                 },
+        //                 {
+        //                     id: actualItem.id
+        //                 }
+        //             ], function (err) {
+        //                 if (err) throw err;
+        //                 console.log("Bid was placed successfuly!");
+        //                 start();
+        //             }
+        //         )
+        //     } else {
+        //         console.log("Your bid was too low! Try again");
+        //         // call rebid here with the items id
+        //         reBid(actualItem.id);
+        //     }
+        // })
+    });
 }
